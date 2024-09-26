1099e135cf5a3bab52ca6cb14209031e49c4a913
xjul-ien <163544042+xjul-ien@users.noreply.github.com>
2024-09-26 18:11:47 +1000

    Update and rename main.js to server.js

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+app.use(bodyParser.json());
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "YOUR_CLIENT_ID",
+  clientSecret: "YOUR_CLIENT_SECRET",
+  redirectUri: "YOUR_REDIRECT_URI"
+});
+
+// Refresh access token
+app.post("/refresh_token", (req, res) => {
+  const { refreshToken } = req.body;
+  spotifyApi.setRefreshToken(refreshToken);
+  spotifyApi.refreshAccessToken().then(
+    function(data) {
+      res.json({
+        accessToken: data.body['access_token']
+      });
+    },
+    function(err) {
+      console.log("Could not refresh access token", err);
+      res.status(500).json({ error: "Failed to refresh token" });
+    }
+  );
+});
+
+// Get currently playing track data
+app.get("/currently-playing", (req, res) => {
+  const accessToken = req.headers['authorization'].split(" ")[1];
+  spotifyApi.setAccessToken(accessToken);
+
+  spotifyApi.getMyCurrentPlaybackState().then(
+    function(data) {
+      if (data.body && data.body.is_playing) {
+        res.json({
+          song: data.body.item,
+          progress: data.body.progress_ms,
+          duration: data.body.item.duration_ms
+        });
+      } else {
+        res.status(204).send(); // No content, nothing is playing
+      }
+    },
+    function(err) {
+      console.log("Something went wrong!", err);
+      res.status(500).json({ error: "Failed to fetch currently playing track" });
+    }
+  );
+});
+
+// Serve audio data (simulate getting playback data)
+app.get("/audio-data", (req, res) => {
+  // Simulate retrieving and sending audio data as frequencies
+  const frequencies = [200, 400, 600, 800, 1000]; // Example frequency data
+  res.json({ frequencies });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
